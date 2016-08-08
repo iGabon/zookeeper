8e4de3a08263e069f63f8227141bf3e98ef433b7
hzyangjiapeng <hzyangjiapeng@corp.netease.com>
2016-08-08 16:50:44 +0800

    Enhance:init repository

---
+++ src/main/java/com/netease/cloud/gabon_test/Master.java
@@ -0,0 +1,12 @@
+package com.netease.cloud.gabon_test;
+
+import org.apache.zookeeper.ZooKeeper;
+
+public class Master {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
