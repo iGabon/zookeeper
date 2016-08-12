750abea5fa99e819f1d75a6e000c0d94c8c6be8b
hzyangjiapeng <hzyangjiapeng@corp.netease.com>
2016-08-12 10:08:56 +0800

    Enhance:add Executor and Monitor

--- src/main/java/com/zookeeper/conf/conf/Master.java
+++ src/main/java/com/zookeeper/conf/conf/Master.java
@@ -1,6 +1,7 @@
 package com.zookeeper.conf.conf;
 
 import java.io.IOException;
+import java.util.Random;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
@@ -14,6 +15,31 @@ import org.slf4j.LoggerFactory;
 public class Master implements Watcher{
 	
 	private static final Logger logger = LoggerFactory.getLogger(Master.class);
+	
+	private boolean isLeader = false;
+	
+	ZooKeeper zk;
+	
+	String serverId = Integer.toHexString(new Random().nextInt());
+	
+	void startZK() throws IOException{
+		zk = new ZooKeeper("59.111.126.208:2181", 6000, new Master());
+	}
+	
+	void runForMaster(){
+		while (true) {
+			try {
+				zk.create(Const.MASTER, serverId.getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+			} catch (KeeperException e) {
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
